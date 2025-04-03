686e9c9df15d85a03924a12d14db54fe75a7f637
Azreil-OFD <fantom2413@gmail.com>
2025-04-03 15:32:27 +0500

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import Nora from '@primeuix/themes/nora';
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@primevue/nuxt-module',
+    '@vueuse/nuxt',
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Nora
+      }
+    }
+
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { data: home } = await useAsyncData(() => queryCollection('content').path('/').first())
+const mouse = reactive(useMouse())
+</script>
+
+<template>
+  <h1>nuxt template</h1>
+  <ContentRenderer v-if="home" :value="home" />
+  <div v-else>Home not found</div>
+  {{ mouse.x }} {{ mouse.y }}
+</template>
